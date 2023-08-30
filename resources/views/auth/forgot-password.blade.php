ae812ee87b8c93227b9ac2b39a6247fc1298751b
SenadK1997 <senad.okt97@gmail.com>
2023-08-30 12:21:02 +0200

    forgot password bg

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -25,7 +25,7 @@ Forgot Password
             @if ($errors->has('email'))
                 <div class="text-red-700 text-sm">{{ $errors->first('email') }}</div>
             @endif
-            <input id="email" name="email" type="email" autocomplete="email" required class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-gray-100">
+            <input id="email" name="email" type="email" autocomplete="email" required class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-gray-200">
           </div>
         </div>
         <div class="mt-2">
