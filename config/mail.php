9110fc564be5414695a1561082bfa90bf2566f9f
SenadK1997 <senad.okt97@gmail.com>
2023-08-30 12:18:10 +0200

    email production done

--- config/mail.php
+++ config/mail.php
@@ -99,7 +99,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'royalfamilynames@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@royalfamilynames.com'),
         'name' => env('MAIL_FROM_NAME', 'Royal Family Name'),
     ],
 
