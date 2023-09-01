9448b44f6397bfabb62df60891807b8a82c38452
SenadK1997 <senad.okt97@gmail.com>
2023-09-01 18:42:30 +0200

    login 419

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,6 +19,7 @@ RoyalFamilyNames - Log in
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form class="space-y-6" action="{{ route('login.post') }}" method="POST">
         @csrf
+        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
